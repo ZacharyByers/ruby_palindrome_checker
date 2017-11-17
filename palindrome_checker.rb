0612f548d7d45ace8326071c6cbcf555f449b08d
Zach Byers <byerszach@gmail.com>
2017-11-17 09:52:58 -0700

    check all the palindromes

---
+++ palindrome_checker.rb
@@ -0,0 +1,37 @@
+require 'pry'
+require 'colorize'
+
+def reversify(string)
+  string_array = string.split('')
+  string_array.delete(' ')
+  string_array.delete(',')
+  string_array.delete('.')
+  string_array.delete('!')
+  string_array.delete('?')
+  string_array.delete("'")
+  reversed_array = []
+  string_array.each { |char| reversed_array.unshift(char) }
+  reversed_string = reversed_array.join('')
+  string = string_array.join('')
+  if string == reversed_string
+    true
+  else
+    false
+  end
+end
+
+while true
+  puts "Enter a phrase, and I'll tell you if it's a palindrome! (or 'q' to quit)".yellow
+  string = gets.strip.downcase
+
+  if string.downcase == 'q'
+    puts "Toodle pip old bean!".cyan
+    exit 0
+  end
+
+  if reversify(string)
+    puts "Yeppers, that's a palindrome!".green
+  else
+    puts "Nope, that's a boring old regular phrase.".red
+  end
+end
